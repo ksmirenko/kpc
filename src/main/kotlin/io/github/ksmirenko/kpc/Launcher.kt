cdd4862c9dd4b07958383a622be1564278f00ab9
Kirill Smirenko <k.smirenko@gmail.com>
2016-07-06 13:21:48 +0600

    Generate a sample class from BF in Launcher

--- src/main/kotlin/io/github/ksmirenko/kpc/Launcher.kt
+++ src/main/kotlin/io/github/ksmirenko/kpc/Launcher.kt
@@ -1,5 +1,18 @@
 package io.github.ksmirenko.kpc
 
+import java.nio.file.Files
+import java.nio.file.Paths
+
 fun main(args : Array<String>) {
     println("Hello, Kotlin!")
+
+    val parser = BrainfuckParser()
+    val compiler = TokenJvmCompiler()
+    val program = "+++[-]+." // prints 0x0001 symbol
+    // generate class
+    val className = "TestClass"
+    val classByteArray = compiler.generateClassByteArray(parser.parse(program), className)
+    // save to file
+    val targetFile = Paths.get("$className.class")
+    Files.write(targetFile, classByteArray)
 }
